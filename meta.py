8606e67fe243daaa4afb1927778ea128ee4a62f3
Baeo Maltinksy <baeo.maltinsky@gmail.com>
2018-11-07 08:29:01 -0800

    modified gitignore

--- meta.py
+++ meta.py
@@ -1,6 +1,8 @@
 from sickle import Sickle
+import json
+import os
 
 sickle = Sickle("http://export.arxiv.org/oai2")
 records = sickle.ListRecords(metadataPrefix="oai_dc", set='cs')
-for i in range(10):
-    print(records.next())
+for r in records:
+    os.mkdir
